8c8d3c4e7095a3deb8f7f0fa1f56806a7f8f0244
Adriana Velásquez <adrianaveme@unisabana.edu.co>
2021-04-25 16:54:55 -0500

    Fix : Implementation

--- src/test/java/SyntheziseTest.java
+++ src/test/java/SyntheziseTest.java
@@ -1,15 +1,12 @@
 import com.github.javafaker.Faker;
 import entities.*;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SyntheziseTest {
@@ -19,25 +16,6 @@ public class SyntheziseTest {
     public SyntheziseTest() {
 
         faker = new Faker(new Locale("en-US"));
-    }
-
-    @BeforeEach
-    public void setup() throws SabanaResearchException{
-        Group group = new Group(faker.team().name());
-        Project wellFormedProject = new Project(faker.team().name(), LocalDate.now().minusDays(10), LocalDate.now().plusDays(10), group);
-        Iteration iteration = new Iteration(faker.team().name(), wellFormedProject);
-        List <Iteration> iterations = new ArrayList<>();
-
-        iterations.add(iteration);
-        ExecutiveSynthesizer es = new ExecutiveSynthesizer(iterations);
-
-        NormalActivity normalActivity = new NormalActivity(faker.team().name(), Activity.ACTIVE_STATE, iteration);
-        normalActivity.addStep(new Step(faker.team().name(), Duration.ofDays(1)));
-
-        NormalActivity activity = new NormalActivity(faker.team().name(), Activity.ACTIVE_STATE, null);
-        activity.addStep(new Step(faker.team().name(), Duration.ofDays(1)));
-        DocumentedActivity documentedActivity = new DocumentedActivity(faker.team().name(), Activity.ACTIVE_STATE, iteration, activity);
-        documentedActivity.addQuestion(new Question(Question.EASY_QUESTION, faker.team().name(), Duration.ofDays(1)));
     }
 
     @Test
